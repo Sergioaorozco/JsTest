7762da280f926e62aae80f7a98f988ac771ac557
Sergio <sergioa.orozcoo@gmail.com>
2020-07-01 00:06:01 -0500

    Archivo js sobre mayoría de edad

---
+++ mayoriaedad.js
@@ -0,0 +1,10 @@
+var age = 16
+
+if (age>18){
+    console.log("Eres mayor de edad, puedes participar")
+} else if(age<12) {
+    console.log("No tienes edad suficiente para participar")
+} else{
+    console.log("Eres adolescente, puedes participar en compañía de un titular")
+}
+
