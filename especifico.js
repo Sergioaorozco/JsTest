458e1503656fd99f4c2ae1436c096d3d3e7414d8
Sergio <sergioa.orozcoo@gmail.com>
2020-06-30 23:53:52 -0500

    Archivos javascript para examen

---
+++ especifico.js
@@ -0,0 +1,6 @@
+var num = [1,2,3,4,5]
+var numatindexone = num[1]
+var numatindexthree = num[3]
+var message = "los elementos a mostrar son " + numatindexone + " y " + numatindexthree
+
+console.log(message)
---
+++ ultimoelem.js
@@ -0,0 +1,6 @@
+var elem = ["primero","segundo","tercero","cuarto"]
+elem.push("soy el último elemento")
+
+for(var a = 0; a < elem.length; a ++) {
+    console.log(elem[a])
+}
