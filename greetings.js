845415214bbb7b1cdccc6bd3dc5b36d13cfe3f86
Sergio <sergioa.orozcoo@gmail.com>
2020-07-02 00:13:45 -0500

    Ejemplos sobre funciones en Javascript

---
+++ greetings.js
@@ -0,0 +1,6 @@
+function greetings(nombre,apellido){
+    console.log("mi nombre es " + nombre +" y mi apellido es " + apellido)
+}
+
+console.log(greetings("Carlos","Saldarriaga"))
+console.log(greetings("María","Salcedo"))
